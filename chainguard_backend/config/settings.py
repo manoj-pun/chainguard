3e07c380f368750bcad8b4bb9775911f8cfedeb9
manoj-pun <manojpun840@gmail.com>
2026-07-13 19:24:45 +0545

    add:throttling

--- chainguard_backend/config/settings.py
+++ chainguard_backend/config/settings.py
@@ -134,6 +134,15 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS":"drf_spectacular.openapi.AutoSchema",
     # "DEFAULT_PAGINATION_CLASS":"rest_framework.pagination.PageNumberPagination",
     # "PAGE_SIZE":1
+
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        "'rest_framework.throttling.UserRateThrottle'"
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/min',
+        'user': '3/min'
+    }
 }
 
 SIMPLE_JWT = {
